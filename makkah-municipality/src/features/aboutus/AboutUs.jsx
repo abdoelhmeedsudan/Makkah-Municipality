e08059a008b274dfb33d5703a27c1f527951283a
abdoelhmeed <abdoelhmeedsudan@gmail.com>
2025-02-22 18:56:10 +0300

    about

--- makkah-municipality/src/features/aboutus/AboutUs.jsx
+++ makkah-municipality/src/features/aboutus/AboutUs.jsx
@@ -18,88 +18,10 @@ import {
     DgaSecondNavHeaderItem,
   } from "platformscode-new-react";
   import '../../../node_modules/platformscode-new-react/dist/style.css'
-
+import {aboutData} from '../../data/data'
 
 function AboutUs() {
-    const data=[
-        {
-          title: "About us",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "information-diamond",
-        },
-        {
-          title: "Organizational Structure",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "hierarchy-square-02",
-        },
-        {
-          title: "Strategy, Policies, and Regulations",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "strategy",
-        },
-        {
-          title: "Budget and Expenditures",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "money-04",
-        },
-        {
-          title: "Tenders and Procurements",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "validation-approval",
-        },
-        {
-          title: "E-Participation",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "user-group",
-        },
-        {
-          title: "Our Partners / Partnerships",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "city-02",
-        },
-        {
-          title: "Sustainable Development",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "plant-01",
-        },
-        {
-          title: "Careers",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "briefcase-06",
-        },
-        {
-          title: "News and Events",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "news-01",
-        },
-        {
-          title: "Contact Us",
-          primaryActionLabel: "Action",
-          secondaryActionLabel: "Action",
-          showSecondaryAction: true,
-          icon: "contact-02",
-        },
-      ]
+   
   return (
     <div className="overflow-hidden">
     <div className="w-full m-auto overflow-hidden bg-[#F7FDF9]">
@@ -121,13 +43,11 @@ function AboutUs() {
 
         <div className="mt-[24px]">
           <h1 className="display-sm-bold text-[#161616] mb-4">
-            About The Entity
+          الأمانة في سطور
           </h1>
           <p className="text-md-regular text-[#161616]">
-            The "About the Entity" page should serve as a landing page,
-            allowing users to click on the "About the Entity" button on the
-            homepage instead of selecting one of the topics displayed in the
-            dropdown menu.
+          يعود تاريخ إنشاء أمانة العاصمة المقدسة إلى عام 1345هـ في عهد جلالة الملك عبد العزيز طيب الله ثراه بعد صدور أمر منه يرحمه الله بضم الهيئات البلدية في مكة المكرمة إلى إدارة واحدة ذات صلاحيات ومسؤوليات محددة
+          بمسمى (أمانة العاصمة) وأطلق على رئيسها مسمى (أمين العاصمة) وفي عام 1397هـ أضيفت كلمة (المقدسة) إلى التسميتین.
           </p>
         </div>
       </div>
@@ -135,7 +55,7 @@ function AboutUs() {
 
     <div className="px-[16px] md:px-[80px] py-[40px]">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-        {data.map(({ title, primaryActionLabel, icon }) => (
+        {aboutData.map(({ title, primaryActionLabel, icon }) => (
           <div className="CustomCard p-[var(--spacing-xl,16px)] rounded-[16px] shadow-custom  bg-white flex flex-col items-start gap-[24px]">
             <div className="icon bg-[var(--Bg-icon-brand-light,#F3FCF6)] rounded-full p-[12px] flex items-center justify-center">
               <DgaIcon
